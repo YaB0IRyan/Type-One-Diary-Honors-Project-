0155bc52f6fcc345b27929b44bc45459957582f2
YaB0IRyan <ryanpcscott@gmail.com>
2023-03-23 06:27:34 +0000

    Navbar working on add entry screen, along with date selection working

--- app/src/main/java/com/example/typeonediary/AddEntryActivity.java
+++ app/src/main/java/com/example/typeonediary/AddEntryActivity.java
@@ -1,14 +1,123 @@
 package com.example.typeonediary;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class AddEntryActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.navigation.NavigationView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class AddEntryActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, DatePickerDialog.OnDateSetListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_entry);
+
+        /* HOOKS */
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        Button button = (Button) findViewById(R.id.buttonDateSelect);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
+
+        /* TOOLBAR */
+        setSupportActionBar(toolbar);
+
+
+        /* NAV DRAWER MENU */
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_add);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String dateSelected = DateFormat.getDateInstance().format(calendar.getTime());
+        TextView textView = (TextView) findViewById(R.id.textViewEntryDate);
+        textView.setText(dateSelected);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent = new Intent();
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                intent = new Intent(AddEntryActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_add:
+                break;
+
+            case R.id.nav_entries:
+                intent = new Intent(AddEntryActivity.this, ViewEntriesActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_averages:
+                intent = new Intent(AddEntryActivity.this, ViewAveragesActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_settings:
+                intent = new Intent(AddEntryActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/typeonediary/DatePickerFragment.java
@@ -0,0 +1,27 @@
+package com.example.typeonediary;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.telecom.Call;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar calender = Calendar.getInstance();
+        int year = calender.get(Calendar.YEAR);
+        int month = calender.get(Calendar.MONTH);
+        int day = calender.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+    }
+}
